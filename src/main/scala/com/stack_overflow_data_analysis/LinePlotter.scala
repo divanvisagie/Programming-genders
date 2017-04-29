37238bb0a80cfb4078855af870cd21209e139f83
Divan Visagie <me@dvisagie.com>
2017-04-29 19:08:51 +0200

    cleanup

--- src/main/scala/com/stack_overflow_data_analysis/LinePlotter.scala
+++ src/main/scala/com/stack_overflow_data_analysis/LinePlotter.scala
@@ -21,22 +21,8 @@ class LinePlotter {
     }
 
     def draw(): Unit = {
-        val data = new DefaultCategoryDataset
-        data.addValue(100.0, ATTENTION, 1)
-        data.addValue(200.0, ATTENTION, 2)
-        data.addValue(300.0, ATTENTION, 3)
-        data.addValue(400.0, ATTENTION, 4)
-        data.addValue(500.0, ATTENTION, 5)
 
-        data.addValue(500.0, MEDITATION, 1)
-        data.addValue(400.0, MEDITATION, 2)
-        data.addValue(300.0, MEDITATION, 3)
-        data.addValue(200.0, MEDITATION, 4)
-        data.addValue(100.0, MEDITATION, 5)
-
-        drawLineGraph(data, "./charts/lineTest.png")
-
-        var genderDataSet = graphDataFromGroup(GenderAgeLine().data())
+        val genderDataSet = graphDataFromGroup(GenderAgeLine().data())
         drawLineGraph(genderDataSet,"./charts/genderAgeLine.png")
     }
 }
